e0bba61fae90cbd33beeabbb0a456d180637009d
Alla Petukhina <allapetukhina@icloud.com>
2022-07-11 16:01:55 +0200

    Transparent pictures and code creating figures for Ch2

--- pyTSA_ExRate/pyTSA_ExRate.py
+++ pyTSA_ExRate/pyTSA_ExRate.py
@@ -5,13 +5,19 @@ x = pd.read_table('ExchRate NZ per UK.txt', header=0)
 dates = pd.date_range('1991', periods = len(x), freq = 'Q')
 x.index = dates; xts = pd.Series(x['xrate'])
 xts.plot(); plt.xlabel('Years')
-plt.ylabel('Exchange rate'); plt.show()
+plt.ylabel('Exchange rate');
+plt.savefig('pyTSA_ExRate_fig2-1.png', dpi = 1200, 
+             bbox_inches ='tight', transparent = True);
+
 # The following is to plot ACF and PACF using statsmodels
 fig = plt.figure()
 plot_acf(xts, lags = 17, ax = fig.add_subplot(2, 1, 1))
 plot_pacf(xts, lags = 17, ax = fig.add_subplot(2, 1, 2))
-fig.tight_layout(pad = 1.5)
-plt.show()
+fig.tight_layout(pad = 1.5);
+plt.savefig('pyTSA_ExRate_fig2-2.png', dpi = 1200, 
+             bbox_inches ='tight', transparent = True);
 # The following is to plot ACF and PACF using PythonTsa
 from PythonTsa.plot_acf_pacf import acf_pacf_fig
 acf_pacf_fig(xts, both = True, lag = 17)
+plt.savefig('pyTSA_ExRate_fig2-3.png', dpi = 1200, 
+             bbox_inches ='tight', transparent = True);
